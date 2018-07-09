794a2d1f99a492fb22651a22d3bd2d25e271f953
hufei <hu7fei2@sina.com>
2018-07-09 22:17:36 +0800

    创建了 H5Test

---
+++ app/src/main/java/com/woobo/crosswalkdemo/view/activity/tests/H5Test.java
@@ -0,0 +1,19 @@
+package com.woobo.crosswalkdemo.view.activity.tests;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by hufei on 2018/7/9.
+ * 654267767@qq.com
+ * des:
+ */
+public class H5Test extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
